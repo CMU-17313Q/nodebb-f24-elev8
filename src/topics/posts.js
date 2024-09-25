2a2f7fc7da1379e383a4db52b7cf8bffeae9e466
mohtelsayed <mtelsaye@LAPTOP-1P1MHBAT>
2024-09-25 22:53:39 +0300

    Fix lint issues

--- src/topics/posts.js
+++ src/topics/posts.js
@@ -4,14 +4,14 @@
 const _ = require('lodash');
 const validator = require('validator');
 const nconf = require('nconf');
-
+// const { post } = require('jquery');
 const db = require('../database');
 const user = require('../user');
 const posts = require('../posts');
 const meta = require('../meta');
 const plugins = require('../plugins');
 const utils = require('../utils');
-const { post } = require('jquery');
+
 
 const backlinkRegex = new RegExp(`(?:${nconf.get('url').replace('/', '\\/')}|\b|\\s)\\/topic\\/(\\d+)(?:\\/\\w+)?`, 'g');
 
@@ -146,16 +146,15 @@ module.exports = function (Topics) {
 					postObj.user.username = validator.escape(String(postObj.handle));
 					postObj.user.displayname = postObj.user.username;
 				}
-				
-				
-				if(postObj.anon === 'true'){
+
+				if (postObj.anon === 'true') {
 					postObj.user = structuredClone(postObj.user);
-					postObj.user.username = "Anonymous";
+					postObj.user.username = 'Anonymous';
 					postObj.user.displayname = 'Anonymous';
 					postObj.user.userslug = 'Anonymous';
 					postObj.user.status = 'away';
 					postObj.user.postcount = 0;
-					postObj.user.topiccount =  0;
+					postObj.user.topiccount = 0;
 					postObj.user.uid = -1;
 					postObj.user['icon:text'] = '*';
 					postObj.user['icon:bgColor'] = '#aaaaaa';
--- src/topics/teaser.js
+++ src/topics/teaser.js
@@ -67,18 +67,18 @@ module.exports = function (Topics) {
 			post.user = users[post.uid];
 			post.timestampISO = utils.toISOString(post.timestamp);
 			tidToPost[post.tid] = post;
-			
-			if (post.anon === 'true'){
+
+			if (post.anon === 'true') {
 				post.user = structuredClone(post.user);
-				post.user.username = "Anonymous";
-				post.user.userslug = "Anonymous";
+				post.user.username = 'Anonymous';
+				post.user.userslug = 'Anonymous';
 				post.user.uid = -1;
-				post.user.displayname = "Anonymous";
+				post.user.displayname = 'Anonymous';
 				post.user['icon:text'] = '*';
 				post.user['icon:bgColor'] = '#aaaaaa';
 			}
-			
 		});
+
 		await Promise.all(postData.map(p => posts.parsePost(p)));
 
 		const { tags } = await plugins.hooks.fire('filter:teasers.configureStripTags', {

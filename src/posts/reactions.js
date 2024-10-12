3c9755f5557c4a4bd78433c9dc5a93a790cd78c0
Ashwaq Taib <ashwaqtaib@Ashwaqs-MacBook-Pro.local>
2024-10-12 23:15:59 +0300

    updated the logic in posts/reactions.js to have methods regarding adding/removing and getting all reactions

--- src/posts/reactions.js
+++ src/posts/reactions.js
@@ -1,19 +1,25 @@
 'use strict';
 
 const db = require('../database');
+const plugins = require('../plugins');
 
-const Reactions = module.exports;
+module.exports = {
+    async addReaction(pid, uid, emoji) {
+        await db.sortedSetAdd(`post:${pid}:reactions:${emoji}`, Date.now(), uid);
+        await plugins.hooks.fire('action:post.reaction.add', { pid, uid, emoji });
+    },
 
-// Add a reaction to a post
-Reactions.addReaction = async function (pid, uid, emoji) {
-    const reactionKey = `post:${pid}:reactions:${emoji}`;
-    await db.sortedSetAdd(reactionKey, Date.now(), uid);
+    async removeReaction(pid, uid, emoji) {
+        await db.sortedSetRemove(`post:${pid}:reactions:${emoji}`, uid);
+        await plugins.hooks.fire('action:post.reaction.remove', { pid, uid, emoji });
+    },
+
+    async getReactions(pid) {
+        const reactions = {};
+        const emojis = await db.getSortedSetRange(`post:${pid}:reactions`, 0, -1);
+        for (const emoji of emojis) {
+            reactions[emoji] = await db.sortedSetCard(`post:${pid}:reactions:${emoji}`);
+        }
+        return reactions;
+    }
 };
-
-// Remove a reaction from a post
-Reactions.removeReaction = async function (pid, uid, emoji) {
-    const reactionKey = `post:${pid}:reactions:${emoji}`;
-    await db.sortedSetRemove(reactionKey, uid);
-};
-
-

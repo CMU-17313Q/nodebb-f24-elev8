86dadf1e4902b18eb390ee3165cb4f366548f386
Ashwaq Taib <ashwaqtaib@Ashwaqs-MacBook-Pro.local>
2024-10-13 00:06:10 +0300

    updated posts/reactions to have real-time synchronization

--- src/posts/reactions.js
+++ src/posts/reactions.js
@@ -2,21 +2,28 @@
 
 const db = require('../database');
 const plugins = require('../plugins');
+const socketIO = require('../socket.io'); // Assuming you have a socket.io instance
 
 module.exports = {
     async addReaction(pid, uid, emoji) {
         await db.sortedSetAdd(`post:${pid}:reactions:${emoji}`, Date.now(), uid);
         await plugins.hooks.fire('action:post.reaction.add', { pid, uid, emoji });
+
+        // Emit WebSocket event
+        socketIO.server.sockets.emit('reaction:add', { pid, uid, emoji });
     },
 
     async removeReaction(pid, uid, emoji) {
         await db.sortedSetRemove(`post:${pid}:reactions:${emoji}`, uid);
         await plugins.hooks.fire('action:post.reaction.remove', { pid, uid, emoji });
+
+        // Emit WebSocket event
+        socketIO.server.sockets.emit('reaction:remove', { pid, uid, emoji });
     },
 
     async getReactions(pid) {
-        const reactions = {};
         const emojis = await db.getSortedSetRange(`post:${pid}:reactions`, 0, -1);
+        const reactions = {};
         for (const emoji of emojis) {
             reactions[emoji] = await db.sortedSetCard(`post:${pid}:reactions:${emoji}`);
         }

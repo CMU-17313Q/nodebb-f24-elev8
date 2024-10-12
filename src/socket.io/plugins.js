167414039f7ebf6157b33ddecafc6312c2d5acd1
Ashwaq Taib <ashwaqtaib@Ashwaqs-MacBook-Pro.local>
2024-10-12 23:10:52 +0300

    updated the socket.io/plugins.js to have a logic for all emojis and not only thr previous three

--- src/socket.io/plugins.js
+++ src/socket.io/plugins.js
@@ -14,22 +14,6 @@ const SocketPlugins = {};
     Be a good lad and namespace your methods.
 */
 
-'use strict';
-
-const SocketPlugins = {};
-
-/*
-    This file is provided exclusively so that plugins can require it and add their own socket listeners.
-
-    How? From your plugin:
-
-        const SocketPlugins = require.main.require('./src/socket.io/plugins');
-        SocketPlugins.myPlugin = {};
-        SocketPlugins.myPlugin.myMethod = function(socket, data, callback) { ... };
-
-    Be a good lad and namespace your methods.
-*/
-
 // Namespace for emoji reactions
 SocketPlugins.emojiReactions = {};
 
